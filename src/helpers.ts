100bec3961d2c73cc9e4b748e82870cdb7d263f6
Guillaume FINE <90453342+Cosmeak@users.noreply.github.com>
2024-11-04 22:45:17 +0100

    feat: move deployCommands in helper
    Use the deployCommands functions inside main to sync command on bot
    startup and update command:deploy with the new function

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,4 +1,6 @@
 import { walk } from "jsr:@std/fs/walk";
+import { commands } from "./commands/index.ts";
+import { bot } from "./bot.ts";
 
 export const importDirectory = async (folder: string) => {
   const files = {};
@@ -8,4 +10,14 @@ export const importDirectory = async (folder: string) => {
     files[Object.keys(file)[0]] = Object.values(file)[0];
   }
   return files;
+};
+
+export const deployCommands = async () => {
+  bot.logger.info("Loading command...");
+  importDirectory("./src/commands/");
+
+  bot.logger.info("Updating commands...");
+  await bot.helpers.upsertGlobalApplicationCommands(commands.array());
+
+  bot.logger.info("Done!");
 };
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
 import { bot } from "./bot.ts";
+import { deployCommands } from "./helpers.ts";
 
+await deployCommands();
 await bot.start();
